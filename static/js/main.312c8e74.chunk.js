a5924b31bc980ae89c78abea39f0d51bde02b42c
thiagodevx <thiago.dev.x@gmail.com>
2021-06-11 11:13:38 -0300

    Updates

---
+++ static/js/main.312c8e74.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgen=this.webpackJsonpgen||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(1),i=t.n(c),r=t(3),s=t.n(r),o=(t(8),t(0)),d=function(){return Object(o.jsx)("div",{children:"Testing Page"})};s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.312c8e74.chunk.js.map
